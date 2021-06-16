83ce865476a937ee64d620253c2688406b3142cb
LiterallyYasuo <dolphinalt7@gmail.com>
2021-06-15 20:13:50 -0700

    removed stupid banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer () {
       <footer className="py-5 bg-dark fixed-bottom">
         <div className="container">
           <p className="m-0 text-center text-white">
-            does anyone know how to get around this stupid browser restriction on autoplay audio i want to blast naruto music when you come here
+            
           </p>
         </div>
       </footer>
